0e934f221b5cb06d79fc25811ed70f4ba80181e9
emanuelsds <carlosemanuel131@outlook.com>
2021-03-16 12:29:06 -0300

    Controller prototype and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+    def user
+        @users = User.all
+        # puts @users.name
+    end
+
+    def add_user
+        User.create(name: params[:user])
+        redirect_to('/')
+    end
+
+    def game
+        @monster = Monster.find(rand(5) + 1)
+        @user = User.find(params[:user])
+    end
+
+    def attack
+        @user = User.find(params[:data][:user])
+        @monster = Monster.find(params[:data][:monster])
+        @user.killed_monsters<<KilledMonster.new(user_id: @user.id, monster_id: @monster.id)
+        @user.deaths<<Death.new(user_id: @user.id)
+        @user.collected_coins<<CollectedCoin.new(value: @monster.coins)
+        redirect_to "/game?user=#{@user.id}"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#user' 
+
+  post '/add_user', to: 'users#add_user'
+  get '/game', to: 'users#game'
+  post '/attack', to: 'users#attack'
+  post '/collect', to: 'users#collect'
+  # resources :users, only: [:game]
 end

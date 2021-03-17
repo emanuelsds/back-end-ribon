6cc317c1c7ec18748a68d57bc36e4dd5427933a7
emanuelsds <carlosemanuel131@outlook.com>
2021-03-17 19:48:11 -0300

    Trophy system prototype

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,24 @@
 class UsersController < ApplicationController
-    def user
-        @users = User.all
-        # puts @users.name
-    end
+  def user
+    @users = User.all
+  end
 
-    def add_user
-        User.create(name: params[:user])
-        redirect_to('/')
-    end
+  def add_user
+    User.create(name: params[:user])
+    redirect_to('/')
+  end
 
-    def game
-        @monster = Monster.find(rand(5) + 1)
-        @user = User.find(params[:user])
-    end
+  def game
+    @monster = Monster.find(rand(5) + 1)
+    @user = User.find(params[:user])
+  end
 
-    def attack
-        @user = User.find(params[:data][:user])
-        @monster = Monster.find(params[:data][:monster])
-        @user.killed_monsters<<KilledMonster.new(user_id: @user.id, monster_id: @monster.id)
-        @user.deaths<<Death.new(user_id: @user.id)
-        @user.collected_coins<<CollectedCoin.new(value: @monster.coins)
-        redirect_to "/game?user=#{@user.id}"
-    end
+  def attack
+    @user = User.find(params[:data][:user])
+    @monster = Monster.find(params[:data][:monster])
+    @user.killed_monsters << KilledMonster.new(user_id: @user.id, monster_id: @monster.id)
+    @user.deaths << Death.new(user_id: @user.id)
+    @user.collected_coins << CollectedCoin.new(user_id: @user.id, value: @monster.coins)
+    redirect_to "/game?user=#{@user.id}"
+  end
 end
--- app/models/death.rb
+++ app/models/death.rb
@@ -1,3 +1,3 @@
 class Death < ApplicationRecord
-    belongs_to :user
+  belongs_to :user
 end

77580fb4b0757ddb425df5dd080f508c60330ad1
emanuelsds <carlosemanuel131@outlook.com>
2021-03-23 22:22:09 -0300

    improving controller and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,21 +4,27 @@ class UsersController < ApplicationController
   end
 
   def add_user
-    User.create(name: params[:user])
+    @user = User.create(name: params[:user], coins: 0)
     redirect_to('/')
   end
 
   def game
+
     @monster = Monster.find(rand(1..5))
-    @user = User.find(params[:user])
+    @user = User.find(params[:id])
   end
 
   def attack
     @user = User.find(params[:data][:user])
-    @monster = Monster.find(params[:data][:monster])
-    @user.killed_monsters << KilledMonster.new(user_id: @user.id, monster_id: @monster.id)
+    @actual_monster = Monster.find(params[:data][:monster])
+    @monster = Monster.find(rand(1..5))
+    @user.killed_monsters << KilledMonster.new(user_id: @user.id, monster_id: @actual_monster.id)
     @user.deaths << Death.new(user_id: @user.id)
-    @user.collected_coins << CollectedCoin.new(user_id: @user.id, value: @monster.coins)
-    redirect_to "/game?user=#{@user.id}"
+    @user.collected_coins << CollectedCoin.new(user_id: @user.id, value: @actual_monster.coins)
+
+    respond_to do |format|
+      format.html { render :game }
+      format.js { render :game }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   root 'users#user'
 
   post '/add_user', to: 'users#add_user'
-  get '/game', to: 'users#game'
+  post '/game', to: 'users#game'
   post '/attack', to: 'users#attack'
-  post '/collect', to: 'users#collect'
-  # resources :users, only: [:game]
 end
